78352a97c451fb0518cd1b4ec1c1541224c4e609
BARRY CURREY <BARRYCURREY@GMAIL.COM>
2021-05-05 20:14:33 -0700

    Added guessing game js to about me

--- js/app.js
+++ js/app.js
@@ -97,4 +97,54 @@ if (lowerCaseHalloween === 'yes' || lowerCaseHalloween === 'y') {
 
 alert('Thanks for visiting my website ' + userName);
 
+// Number guessing game
 
+let counter1 = 3;
+
+while (counter1 > -1) {
+  let userGuess = prompt('Guess a number between 1 and 10');
+  if (userGuess == 7) {
+    alert('Correct!');
+    counter1 = -1;
+  } else if (userGuess > 7) {
+    alert('Too high. You have ' + counter1 + ' more guesses');
+  } else if (userGuess < 7) {
+    alert('Too low. You have ' + counter1 + ' more guesses');
+  }
+  counter1--;
+}
+alert('The correct answer was 7');
+
+// Movie guessing game
+
+let pointArray = [] // Creates an array for the number attempts
+let correctMovies = ['scott pilgrim', 'ghost world'] // Correct answers
+let counter = 6; // Counter for 6 guesses
+let rightAnswer = false; // Switches the value for correct answer
+
+while (rightAnswer === false && counter > 0) { // While loop. if var right Answer strictly equal to false and counter greater than 0 then run next line. Both conditions must be true.
+
+  let userGuess1 = prompt('Which movies are based on graphic novels?'); // Prompt for user guess
+  let userGuess = userGuess1.toLowerCase(); // changes user answer to lowercase
+
+  for (let i = 0; i < correctMovies.length; i++) { // for loop. Establishes the variable i
+
+    if (userGuess === correctMovies[i]) { // if var userGuess is strictly equal to any of the correctMovies array the run next line
+      alert('Correct!') // Correct alert
+      rightAnswer = true; // Right answer equals true stops the loop
+      alert('Congratulations! You had ' + (pointArray.length) + ' wrong answers.') // Alert used pointArray length to tell the user how many attempts they had
+      alert('Possible answers were: ' + correctMovies [0] + ' and '+ correctMovies [1]); // alert to tell the user the correct answers from correctMovies array
+    }
+  }
+
+  if (rightAnswer === false) { // if right answer is found to be false then an alert follows and the while loop is run again
+    alert('Incorrect.'); // alert for incorrect
+
+  }
+  counter--; //decrement counter (Counts down by 1)
+
+  pointArray.push(counter); // this pushes the counter to a new array called pointArray
+
+  console.log(pointArray) // console.log check
+
+}
